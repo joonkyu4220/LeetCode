9746fe058da088baddfeb149f1d37cac1321b507
joonkyu4220 <67729340+joonkyu4220@users.noreply.github.com>
2021-02-13 01:20:08 +0900

    Update Palindrome Linked List.py

--- ListNode/Palindrome Linked List.py
+++ ListNode/Palindrome Linked List.py
@@ -9,8 +9,36 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        mem = []
-        while (head):
-            mem.append(head.val)
-            head = head.next
-        return mem==mem[::-1]
+        # mem = []
+        # while (head):
+        #     mem.append(head.val)
+        #     head = head.next
+        # return mem==mem[::-1]
+        
+        if not(head):
+            return True
+        
+        fast = head
+        slow = head
+        while (fast.next and fast.next.next):
+            fast = fast.next.next
+            slow = slow.next
+        
+        prev_ = None
+        cur_ = slow.next
+        while (cur_):
+            next_ = cur_.next
+            cur_.next = prev_
+            prev_ = cur_
+            cur_ = next_
+        
+        tail = prev_
+        
+        while (tail):
+            if head.val != tail.val:
+                return False
+            else:
+                tail = tail.next
+                head = head.next
+            
+        return True
