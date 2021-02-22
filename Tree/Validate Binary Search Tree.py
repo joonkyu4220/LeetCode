4b55c3b43c6091622345babd3c763df1f7c6e7e0
joonkyu4220 <67729340+joonkyu4220@users.noreply.github.com>
2021-02-22 22:38:20 +0900

    Update Validate Binary Search Tree.py

--- Tree/Validate Binary Search Tree.py
+++ Tree/Validate Binary Search Tree.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/625/
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,57 +12,71 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        if self.maxVal(root)[0] and self.minVal(root)[0]:
-            return True
-        else:
-            return False
+        
+        maxi, mini = float('inf'), float('-inf')
+        
+        def traverse(root, maxi, mini):
+            if root == None:
+                return True
+            l = traverse(root.left, root.val, mini)
+            r = traverse(root.right, maxi, root.val)
+            
+            return l and r and (root.val > mini) and (root.val < maxi)
+        
+        return traverse(root, maxi, mini)
+        
+        
+#         def maxVal(root):
+#             if not(root):
+#                 return (True, None)
+#             elif not(root.left) and not(root.right):
+#                 return (True, root.val)
+#             elif not(root.left):
+#                 rightMax = maxVal(root.right)
+#                 if rightMax[0] and rightMax[1] > root.val:
+#                     return (True, rightMax[1])
+#                 else:
+#                     return (False, None)
+#             elif not(root.right):
+#                 leftMax = maxVal(root.left)
+#                 if leftMax[0] and leftMax[1] < root.val:
+#                     return (True, root.val)
+#                 else:
+#                     return (False, None)
+#             else:
+#                 rightMax = maxVal(root.right)
+#                 leftMax = maxVal(root.left)
+#                 if rightMax[0] and leftMax[0] and leftMax[1] < root.val < rightMax[1]:
+#                     return (True, rightMax[1])
+#                 else:
+#                     return (False, None)
     
-    def maxVal(self, root):
-        if not(root):
-            return (True, None)
-        elif not(root.left) and not(root.right):
-            return (True, root.val)
-        elif not(root.left):
-            rightMax = self.maxVal(root.right)
-            if rightMax[0] and rightMax[1] > root.val:
-                return (True, rightMax[1])
-            else:
-                return (False, None)
-        elif not(root.right):
-            leftMax = self.maxVal(root.left)
-            if leftMax[0] and leftMax[1] < root.val:
-                return (True, root.val)
-            else:
-                return (False, None)
-        else:
-            rightMax = self.maxVal(root.right)
-            leftMax = self.maxVal(root.left)
-            if rightMax[0] and leftMax[0] and leftMax[1] < root.val < rightMax[1]:
-                return (True, rightMax[1])
-            else:
-                return (False, None)
-    
-    def minVal(self, root):
-        if not(root):
-            return (True, None)
-        elif not(root.left) and not(root.right):
-            return (True, root.val)
-        elif not(root.left):
-            rightMin = self.minVal(root.right)
-            if rightMin[0] and rightMin[1] > root.val:
-                return (True, root.val)
-            else:
-                return (False, None)
-        elif not(root.right):
-            leftMin = self.minVal(root.left)
-            if leftMin[0] and leftMin[1] < root.val:
-                return (True, leftMin[1])
-            else:
-                return (False, None)
-        else:
-            rightMin = self.minVal(root.right)
-            leftMin = self.minVal(root.left)
-            if rightMin[0] and leftMin[0] and leftMin[1] < root.val < rightMin[1]:
-                return (True, leftMin[1])
-            else:
-                return (False, None)
+#         def minVal(root):
+#             if not(root):
+#                 return (True, None)
+#             elif not(root.left) and not(root.right):
+#                 return (True, root.val)
+#             elif not(root.left):
+#                 rightMin = minVal(root.right)
+#                 if rightMin[0] and rightMin[1] > root.val:
+#                     return (True, root.val)
+#                 else:
+#                     return (False, None)
+#             elif not(root.right):
+#                 leftMin = minVal(root.left)
+#                 if leftMin[0] and leftMin[1] < root.val:
+#                     return (True, leftMin[1])
+#                 else:
+#                     return (False, None)
+#             else:
+#                 rightMin = minVal(root.right)
+#                 leftMin = minVal(root.left)
+#                 if rightMin[0] and leftMin[0] and leftMin[1] < root.val < rightMin[1]:
+#                     return (True, leftMin[1])
+#                 else:
+#                     return (False, None)
+                
+#         if maxVal(root)[0] and minVal(root)[0]:
+#             return True
+#         else:
+#             return False
