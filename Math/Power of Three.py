eaa6379658dc6c6ad05dfee900f5ce88c8539706
joonkyu4220 <67729340+joonkyu4220@users.noreply.github.com>
2021-03-10 02:18:22 +0900

    Update Power of Three.py

--- Math/Power of Three.py
+++ Math/Power of Three.py
@@ -4,9 +4,20 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n == 1:
-            return True
-        elif n <= 0 or n % 3 != 0:
+        # if n == 1:
+        #     return True
+        # elif n <= 0 or n % 3 != 0:
+        #     return False
+        # else:
+        #     return self.isPowerOfThree(n / 3)
+        
+        if n <= 0:
             return False
-        else:
-            return self.isPowerOfThree(n / 3)
+        
+        while True:
+            if n == 1:
+                return True
+            elif n % 3:
+                return False
+            else:
+                n /= 3
