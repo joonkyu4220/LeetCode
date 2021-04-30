c1f9cdcd8bc75756a46807ef00905c87c4b59c37
joonkyu4220 <67729340+joonkyu4220@users.noreply.github.com>
2021-04-30 17:31:38 +0900

    Create Kth Largest Element in an Array.py

---
+++ Kth Largest Element in an Array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        r = [-10001 for _ in range(k)]
+        
+        for n in nums:
+            for i in range(k):
+                if r[i] < n:
+                    r[i+1:] = r[i:-1]
+                    r[i] = n 
+                    break
+        return r[k-1]
+                    
