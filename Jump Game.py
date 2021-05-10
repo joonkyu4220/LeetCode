662bf73103785ac94559e11246d688a25b4e0af8
joonkyu4220 <67729340+joonkyu4220@users.noreply.github.com>
2021-05-10 14:30:58 +0900

    Update Jump Game.py

--- Jump Game.py
+++ Jump Game.py
@@ -4,22 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        numNums = len(nums)
-        if numNums == 1:
+        
+        cur = len(nums) - 2
+        if cur == - 1:
             return True
-        possible = [0]
-        maxPoss = 0
-        numPoss = 1
-        cur = 0
-        while cur < numPoss:
-            curPoss = possible[cur]
-            n = nums[curPoss]
-            if maxPoss < curPoss + n:
-                if curPoss + n >= numNums - 1:
-                    return True
-                possible = possible + [i for i in range(maxPoss + 1, curPoss+ n + 1)]
-                numPoss += curPoss + n - maxPoss
-                maxPoss = curPoss + n
-            cur += 1
-        return False
-                
+        minFlag = len(nums) - 1
+        while cur >= 0:
+            if cur + nums[cur] >= minFlag:
+                minFlag = cur
+            cur -= 1
+        if minFlag == 0:
+            return True
+        else:
+            return False
+        
+        
+        # numNums = len(nums)
+        # if numNums == 1:
+        #     return True
+        # possible = [0]
+        # maxPoss = 0
+        # numPoss = 1
+        # cur = 0
+        # while cur < numPoss:
+        #     curPoss = possible[cur]
+        #     n = nums[curPoss]
+        #     if maxPoss < curPoss + n:
+        #         if curPoss + n >= numNums - 1:
+        #             return True
+        #         possible = possible + [i for i in range(maxPoss + 1, curPoss+ n + 1)]
+        #         numPoss += curPoss + n - maxPoss
+        #         maxPoss = curPoss + n
+        #     cur += 1
+        # return False
