557eb695a7024f570a8aeacc363049047c2ea38a
joonkyu4220 <67729340+joonkyu4220@users.noreply.github.com>
2021-05-22 22:15:18 +0900

    Update Unique Paths.py

--- Unique Paths.py
+++ Unique Paths.py
@@ -5,12 +5,20 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        m, n = max(m, n), min(m, n)
-        mi = m-1
-        ni = n-1
-        ans = 1
-        while ni >= 1:
-            ans *= (mi + ni)
-            ans /= (n - ni)
-            ni -= 1
-        return ans
+        l = [1 for _ in range(n)]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                l[j] += l[j-1]
+        return l[-1]
+                
+        
+        # m, n = max(m, n), min(m, n)
+        # mi = m-1
+        # ni = n-1
+        # ans = 1
+        # while ni >= 1:
+        #     ans *= (mi + ni)
+        #     ans /= (n - ni)
+        #     ni -= 1
+        # return ans
